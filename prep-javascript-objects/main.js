b697cc8d7c6983aedddff1bdbab3709cfd507da4
Patricia Manalang <patriciamanalang06@gmail.com>
2022-08-09 20:18:41 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,18 @@
+var person = {
+  firstName: 'Patricia',
+  lastName: 'Manalang',
+  hobby: 'Watching TikToks'
+};
+console.log(person);
+var firstName = 'Patricia ';
+var lastName = 'Manalang ';
+var fullName = firstName + lastName;
+console.log("The person's name is:", fullName);
+
+person.job = 'realtor';
+console.log("The person's current job is:", person.job);
+
+person.previousJob = 'college student';
+console.log("The person's previous job is:", person.previousJob);
+
+console.log('The complete person object:', person);
