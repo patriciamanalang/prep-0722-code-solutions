c92cd77c8d381b8123710157ec93c66dfbdce982
Patricia Manalang <patriciamanalang06@gmail.com>
2022-08-11 02:52:20 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,11 @@
+// create your loops here.
+
+for (var k = 0; k < 10; k++) {
+  console.log(k);
+}
+for (var i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+for (var e = 100; e > 0; e--) {
+  console.log('Time till explosion:', e);
+}
